46b00e629e068927ef27c7f6c5dd4ca503951408
nonogon <leslie.strauss@gmail.com>
2014-09-17 17:12:19 -0700

    finished partialRight tests and function

--- index.js
+++ index.js
@@ -3,12 +3,34 @@
 /**
  * Document me, please!
  */
+
 module.exports.partial = function(fn) {
   var args = Array.prototype.slice.call(arguments);
   var partialArgs = args.slice(1);
   return function() {
     var args2 = Array.prototype.slice.call(arguments);
     partialArgs = partialArgs.concat(args2);
-    return fn.apply({}, partialArgs);
+    return fn.apply(undefined, partialArgs);
   };
+};
+
+
+
+module.exports.partialRight = function(fn) {
+  var args = Array.prototype.slice.call(arguments);
+  var partialArgs = args.slice(1);
+  return function() {
+    var args2 = Array.prototype.slice.call(arguments);
+    partialArgs = args2.concat(partialArgs);
+    return fn.apply(undefined, partialArgs);
+  };
+};
+
+
+
+
+var util = require('util');
+module.exports.steve = function(name, hobby, frequency) {
+  return util.format('%s likes to %s every %s days', name, hobby, frequency);
+
 };
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,4 @@
+"use strict";
 var expect = require('chai').expect;
 var util = require('util');
 var lib = require('../index');
@@ -30,6 +31,46 @@ describe('partial', function() {
     var hiWhitney = lib.partial(greet, 'Hi', 'Whitney');
     var result = hiWhitney('Nunn');
     expect(result).to.eql('Whitney Nunn says Hi');
+  });
+});
+
+
+
+
+describe('partialRight', function() {
+  it('works for 2 arguments', function() {
+    var greet = function(greeting, name) {
+      return util.format('%s says %s', name, greeting);
+    };
+    var greetLeslie = lib.partialRight(greet, 'Leslie');
+    var result = greetLeslie('Hola');
+    expect(result).to.eql('Leslie says Hola');
+  });
+
+  it('works for 2 arguments (again)', function() {
+    var greet = function(name, greeting) {
+      return util.format('%s says %s', name, greeting);
+    };
+    var hi = lib.partialRight(greet, 'Hi');
+    var result = hi('Kali');
+    expect(result).to.eql('Kali says Hi');
+  });
+});
+
+
+
+
+describe('questions', function() {
+
+  it('helps steve answer questions', function() {
+    var statement = lib.steve('Steve', 'run', 1);
+    expect(statement).to.eql('Steve likes to run every 1 days');
+  });
+
+  it('helps steve answer questions', function() {
+    var whitGaveYouThisArray = ['Steve', 'run', 1];
+    var statement = lib.steve.apply(undefined, whitGaveYouThisArray);
+    expect(statement).to.eql('Steve likes to run every 1 days');
   });
 
 });
